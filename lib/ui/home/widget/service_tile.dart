92262fb9f28e4563293e99a4587f1c4e428bd460
roshan <rijalroshan94@gmail.com>
2021-07-01 09:21:43 +0545

    init repo

---
+++ lib/ui/home/widget/service_tile.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+import 'characteristic_tile.dart';
+
+
+class ServiceTile extends StatelessWidget {
+  final BluetoothService service;
+  final List<CharacteristicTile> characteristicTiles;
+
+  const ServiceTile(
+      {Key? key, required this.service, required this.characteristicTiles})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (characteristicTiles.length > 0) {
+      return ExpansionTile(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Service'),
+            Text('0x${service.uuid.toString().toUpperCase().substring(4, 8)}',
+                style: Theme.of(context).textTheme.body1?.copyWith(
+                    color: Theme.of(context).textTheme.caption?.color))
+          ],
+        ),
+        children: characteristicTiles,
+      );
+    } else {
+      return ListTile(
+        title: Text('Service'),
+        subtitle:
+        Text('0x${service.uuid.toString().toUpperCase().substring(4, 8)}'),
+      );
+    }
+  }
+}
