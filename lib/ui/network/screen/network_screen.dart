67ceafc891fdcd378a8a7ac61500c6090c8d4852
Derek Yeh <derek.yeh@yale.edu>
2021-08-06 20:00:48 -0700

    add commands to network

--- lib/ui/network/screen/network_screen.dart
+++ lib/ui/network/screen/network_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class NetWorkScreen extends StatefulWidget {
@@ -7,7 +6,7 @@ class NetWorkScreen extends StatefulWidget {
 }
 
 class _NetWorkScreenState extends State<NetWorkScreen> {
-  bool sharingData = false;
+  static int networkMode = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,41 +17,51 @@ class _NetWorkScreenState extends State<NetWorkScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Card(
-                    color: Colors.redAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: SizedBox(
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(Icons.wifi, color: Colors.white, size: 50),
-                          Text('Wifi', style: TextStyle(fontSize: 20, color: Colors.white))
-                        ],
+                InkWell(
+                  onTap: () => setState(() => networkMode = 1),
+                  child: Card(
+                      elevation: (networkMode == 1 ? 50: 0),
+                      shadowColor: (networkMode == 1 ? Colors.redAccent: null),
+                      color: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: SizedBox(
+                          width: 125,
+                          height: 125,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.wifi, color: Colors.white, size: 50),
+                              Text('Wifi', style: TextStyle(fontSize: 20, color: Colors.white))
+                            ],
+                          )
                       )
-                    )
+                  )
                 ),
-                Card(
-                    color: Colors.lightBlueAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: SizedBox(
-                        width: 125,
-                        height: 125,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.speaker_phone, color: Colors.white, size: 50),
-                            Text('Hotspot', style: TextStyle(fontSize: 20, color: Colors.white))
-                          ],
-                        )
-                    )
+                InkWell(
+                    onTap: () => setState(() => networkMode = 2),
+                  child: Card(
+                      elevation: (networkMode == 2 ? 50: 0),
+                      shadowColor: (networkMode == 2 ? Colors.lightBlueAccent: null),
+                      color: Colors.lightBlueAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: SizedBox(
+                          width: 125,
+                          height: 125,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.speaker_phone, color: Colors.white, size: 50),
+                              Text('Hotspot', style: TextStyle(fontSize: 20, color: Colors.white))
+                            ],
+                          )
+                      )
+                  )
                 ),
               ],
             ),
@@ -60,44 +69,83 @@ class _NetWorkScreenState extends State<NetWorkScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Card(
-                    color: Colors.pinkAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: SizedBox(
-                        width: 125,
-                        height: 125,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.share, color: Colors.white, size: 50),
-                            Text('Bridge', style: TextStyle(fontSize: 20, color: Colors.white))
-                          ],
+                InkWell(
+                    onTap: () => setState(() => networkMode = 3),
+                    child: Card(
+                        elevation: (networkMode == 3 ? 50: 0),
+                        shadowColor: (networkMode == 3 ? Colors.pinkAccent: null),
+                        color: Colors.pinkAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: SizedBox(
+                            width: 125,
+                            height: 125,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(Icons.share, color: Colors.white, size: 50),
+                                Text('Bridge', style: TextStyle(fontSize: 20, color: Colors.white))
+                              ],
+                            )
                         )
                     )
                 ),
-                Card(
-                    color: Colors.amber,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: SizedBox(
-                        width: 125,
-                        height: 125,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.cable, color: Colors.white, size: 50),
-                            Text('Ethernet', style: TextStyle(fontSize: 20, color: Colors.white))
-                          ],
-                        )
-                    )
-                ),
+                InkWell(
+                    onTap: () => setState(() => networkMode = 4),
+                  child: Card(
+                      elevation: (networkMode == 4 ? 50: 0),
+                      shadowColor: (networkMode == 4 ? Colors.amber: null),
+                      color: Colors.amber,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: SizedBox(
+                          width: 125,
+                          height: 125,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.cable, color: Colors.white, size: 50),
+                              Text('Ethernet', style: TextStyle(fontSize: 20, color: Colors.white))
+                            ],
+                          )
+                      )
+                  )
+                )
               ],
-            )
+            ),
+            SizedBox(height: 30),
+            Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                child: Column(
+                    children: <Widget> [
+                      ExpansionTile(
+                          title: Text("Commands"),
+                          trailing: Icon(Icons.keyboard_arrow_right),
+                          children: [
+                            ListTile(
+                                title: Text("Update Network Mode")
+                            ),
+                            ListTile(
+                                title: Text("Reboot Raspberry Pi")
+                            ),
+                            ListTile(
+                                title: Text("Reset Network")
+                            ),
+                            ListTile(
+                                title: Text("Speed Test")
+                            ),
+                            ListTile(
+                                title: Text("Reverse Lookup")
+                            ),
+                          ],
+                      )
+                    ]
+                )
+            ),
           ]
         )
       )
---
+++ lib/ui/network/widget/network_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NetWorkCard extends StatefulWidget {
+  final int cardNum;
+  final Color color;
+  final IconData icon;
+  final String type;
+  NetWorkCard(this.cardNum, this.color, this.icon, this.type);
+  @override
+  State<NetWorkCard> createState() => _NetWorkCard();
+}
+
+class _NetWorkCard extends State<NetWorkCard> {
+  int networkMode = 0;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => setState(() => networkMode = widget.cardNum),
+      child: Card(
+          elevation: (networkMode == widget.cardNum ? 50: 0),
+          shadowColor: (networkMode == widget.cardNum ? widget.color: null),
+          color: widget.color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: SizedBox(
+              width: 125,
+              height: 125,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(widget.icon, color: Colors.white, size: 50),
+                  Text(widget.type, style: TextStyle(fontSize: 20, color: Colors.white))
+                ],
+              )
+          )
+      )
+    );
+  }
+}
