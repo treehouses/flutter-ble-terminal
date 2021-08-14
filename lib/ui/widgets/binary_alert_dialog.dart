9c9fbf72e30ac5c129bdaf1c1f84801c15102d4d
Derek Yeh <djyeh@uci.edu>
2021-08-13 17:02:00 -0700

    add initial settings screen (#7)

---
+++ lib/ui/widgets/binary_alert_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class BinaryAlertDialog {
+  static BinaryAlertDialog _instance = new BinaryAlertDialog();
+  factory BinaryAlertDialog() => _instance;
+
+  static void show(BuildContext context, String title, String content, {String leftButtonText = "No", String rightButtonText = "Yes"}) {
+    Widget noButton = TextButton(
+        child: Text(leftButtonText),
+        onPressed:  () => Navigator.of(context).pop()
+    );
+    Widget yesButton = TextButton(
+        child: Text(rightButtonText),
+        onPressed:  () => Navigator.of(context).pop()
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: [
+        noButton,
+        yesButton,
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+  }
+}
