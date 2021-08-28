59acd96ec9193cac086b2d9fb9116db303be3986
rrijal53 <rijalroshan94@gmail.com>
2021-08-28 09:14:07 +0545

    cleaan up assets

--- lib/common/constants/assets.dart
+++ lib/common/constants/assets.dart
@@ -1,58 +1,5 @@
 class Assets {
   static const imagePath = "assets/images/";
   static const iconsPath = "assets/icons/";
-  static const galaxy_placeholder_url =
-      "https://www.galaxytvnepal.com/assets/img/banner-1.jpg";
 
-  static const userPlaceholderImage =
-      "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
-  static const String placeholder = imagePath + "placeholder.png";
-  static const ads =
-      "https://wordstream-files-prod.s3.amazonaws.com/s3fs-public/images/media/images/persuasive-ads-coca-cola.jpg";
-  static const String testImage = galaxy_placeholder_url;
-  // "https://i.pinimg.com/originals/c4/2c/d3/c42cd325eb9e88ff4acd0b6914b9a3f0.jpg";
-
-  static String textVideo =
-      "https://logos-channel.scaleengine.net/logos-channel/live/biblescreen-ad-free/playlist.m3u8";
-  static const liveTvUrl =
-      "https://logos-channel.scaleengine.net/logos-channel/live/biblescreen-ad-free/playlist.m3u8";
-  static const signUpBg = imagePath + "signup_bg.png";
-  static const splash = imagePath + "splash.png";
-
-  static const onboard_splash = imagePath + "onboard_1.png";
-  static const onboard_news = imagePath + "onboard_news.png";
-  static const onboard_shows = imagePath + "onboard_shows.png";
-  static const onobard_tv = imagePath + "onboard_tv.png";
-  static const logo = imagePath + "logo.png";
-
-  static const nepal_flag = imagePath + "np.svg";
-  static const britain_flag = imagePath + "GBP.svg";
-  static const like_icon = iconsPath + "like.svg";
-  static const dislike_icon = iconsPath + "dislike.svg";
-
-  static const galaxy_placeholder_asset = imagePath + "galaxy_placeholder.jpg";
-
-  static const galaxyTextLogoSvg = imagePath + "galaxy_logo.svg";
-  static const galaxyDrawerLogoSvg = imagePath + "galaxy_red.svg";
-
-  static const sahiGalatLogo = imagePath + "sahi_galat_logo.png";
-  static const sahi = imagePath + "sahi.svg";
-  static const galat = imagePath + "galat.svg";
-  static const sound = imagePath + "sound.svg";
-
-  static const shareWatermark = imagePath + "share_watermark.png";
-  static const oops = imagePath + "oops.png";
-
-  /// animations
-
-  static const left_hand = "assets/animations/hand_left.json";
-  static const right_hand = "assets/animations/hand_right.json";
-
-  /// socials
-
-  static const facebookIcon = iconsPath + "facebook.svg";
-  static const instagramIcon = iconsPath + "instagram.svg";
-  static const liveTvIcon = iconsPath + "television.svg";
-  static const twitterIcon = iconsPath + "twitter.svg";
-  static const tiktokIcon = iconsPath + "tiktok.svg";
 }
