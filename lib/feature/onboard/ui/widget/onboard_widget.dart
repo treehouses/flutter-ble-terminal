138511c3c71dbdcd7434d8f46b9819353256862f
Roshan Rijal <user.name=roshan>
2021-08-28 09:12:05 +0545

    onboarding page

--- lib/feature/onboard/ui/widget/onboard_widget.dart
+++ lib/feature/onboard/ui/widget/onboard_widget.dart
@@ -7,29 +7,38 @@ class OnboardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageWrapper(
-      body: Center(child: Column(children: [Text("GET STARTED") ,Center(
-        child:  InkWell(
-          splashColor: Colors.transparent,
-          customBorder: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(45)),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, Routes.dashboard);
-          },
-          child: Container(
-            height: 65,
-            width: 65,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: CustomTheme.primaryColor,
-            ),
-            child: Icon(
-              Icons.arrow_forward,
-              color: Colors.white,
-              size: 45,
-            ),
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("GET STARTED"),
+            Center(
+              child: InkWell(
+                splashColor: Colors.transparent,
+                customBorder: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(45)),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, Routes.dashboard);
+                },
+                child: Container(
+                  height: 65,
+                  width: 65,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: CustomTheme.primaryColor,
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                    size: 45,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-      )],),),
+      ),
     );
   }
 }
