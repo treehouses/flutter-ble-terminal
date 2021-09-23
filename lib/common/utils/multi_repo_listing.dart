0e200e5114bd52b9e020a1b23b060d6a6bdbace2
Roshan Rijal <rijalroshan94@gmail.com>
2021-09-23 09:34:30 +0545

    bloc pattern implemented for search (#34)

--- lib/common/utils/multi_repo_listing.dart
+++ lib/common/utils/multi_repo_listing.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:treehousesble/common/constants/env.dart';
 
 import 'multi_bloc_listing.dart';
 
 class MultiRepoListing extends StatelessWidget {
   final Widget child;
-  const MultiRepoListing({required this.child});
+  final Env env;
+
+  const MultiRepoListing({required this.child, required this.env});
+
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(providers: [], child: MultiBlocListing(child: child));
+    return MultiRepositoryProvider(providers: [
+      RepositoryProvider<Env>(create: (context) => env, lazy: true),
+    ], child: MultiBlocListing(child: child));
   }
 }
--- lib/feature/dashboard/screen/dashboard_screen.dart
+++ lib/feature/dashboard/screen/dashboard_screen.dart
@@ -1,6 +1,10 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:treehousesble/common/bloc/bluetooth_cubit.dart';
+import 'package:treehousesble/common/bloc/bluetooth_state.dart';
+import 'package:treehousesble/common/navigation/nav.dart';
+import 'package:treehousesble/common/route/routes.dart';
 import 'package:treehousesble/feature/dashboard/screen/search_screen.dart';
 import 'package:treehousesble/feature/dashboard/widget/fab_bottom_app_bar.dart';
 import 'package:treehousesble/feature/network/screen/network_screen.dart';
@@ -14,10 +18,33 @@ class FindDevicesScreen extends StatefulWidget {
 class _FindDevicesScreenState extends State<FindDevicesScreen> {
   int pageIndex = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    context.read<BluetoothCubit>()..checkDeviceConnected();
+  }
+
+  Widget getHome() {
+    return BlocConsumer(
+        bloc: context.read<BluetoothCubit>(),
+        listener: (con, state) {
+
+        },
+        builder: (context, state) {
+          if (state is StateDeviceConnected) {
+            return Text("Connected to device " +
+                state.characteristic.deviceId.toString());
+          } else if (state is StateDeviceNotConnected) {
+            return Text("Not connected to device");
+          }
+          return Container();
+        });
+  }
+
   Widget pages(int index) {
-    switch(index) {
+    switch (index) {
       case 0:
-        return SearchPage();
+        return getHome();
       case 1:
         return Container();
       case 2:
@@ -30,7 +57,7 @@ class _FindDevicesScreenState extends State<FindDevicesScreen> {
   }
 
   String pageName(int index) {
-    switch(index) {
+    switch (index) {
       case 0:
         return "Home";
       case 1:
@@ -44,7 +71,6 @@ class _FindDevicesScreenState extends State<FindDevicesScreen> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,40 +83,29 @@ class _FindDevicesScreenState extends State<FindDevicesScreen> {
         stream: FlutterBlue.instance.isScanning,
         initialData: false,
         builder: (c, snapshot) {
-          if (snapshot.data!) {
-            return FloatingActionButton(
-              child: Icon(Icons.stop),
-              onPressed: () => FlutterBlue.instance.stopScan(),
-              backgroundColor: Colors.red,
-            );
-          } else {
-            return FloatingActionButton(
-                child: Icon(Icons.search),
-                onPressed: () => FlutterBlue.instance
-                    .startScan(timeout: Duration(seconds: 10)));
-          }
+          return FloatingActionButton(
+                    child: Icon(Icons.search),
+                    onPressed: () => Navigator.pushNamed(context, Routes.search));
         },
       ),
       bottomNavigationBar: FABBottomAppBar(
-        items: [
-          FABBottomAppBarItem(iconData: Icons.dashboard, text: 'Home'),
-          FABBottomAppBarItem(iconData: Icons.branding_watermark_outlined, text: 'Terminal'),
-          FABBottomAppBarItem(iconData: Icons.network_wifi, text: 'Network'),
-          FABBottomAppBarItem(iconData: Icons.settings, text: 'Settings'),
-        ],
-        notchedShape: CircularNotchedRectangle(),
-        color: Colors.black54,
-        selectedColor: Colors.white,
-        centerItemText: '',
-        backgroundColor: Theme.of(context).primaryColor,
-        onTabSelected: (int value) {
-          setState(() {
-            pageIndex = value;
-          });
-        }
-    ),
+          items: [
+            FABBottomAppBarItem(iconData: Icons.dashboard, text: 'Home'),
+            FABBottomAppBarItem(
+                iconData: Icons.branding_watermark_outlined, text: 'Terminal'),
+            FABBottomAppBarItem(iconData: Icons.network_wifi, text: 'Network'),
+            FABBottomAppBarItem(iconData: Icons.settings, text: 'Settings'),
+          ],
+          notchedShape: CircularNotchedRectangle(),
+          color: Colors.black54,
+          selectedColor: Colors.white,
+          centerItemText: '',
+          backgroundColor: Theme.of(context).primaryColor,
+          onTabSelected: (int value) {
+            setState(() {
+              pageIndex = value;
+            });
+          }),
     );
   }
-
-
 }
