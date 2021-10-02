5f26b1d4004d0bdb99cfc381d33acf4531decbd9
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2021-10-02 11:53:17 +0800

    push

--- lib/feature/onboard/ui/screen/onboard_page.dart
+++ lib/feature/onboard/ui/screen/onboard_page.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import '../widget/onboard_widget.dart';
 
 class OnboardPage extends StatelessWidget {
+  late PageController _pageController;
+  int currentIndex = 0;
+
+
   @override
   Widget build(BuildContext context) {
-    return OnboardWidget();
+    return PageView(
+            controller: _pageController,
+            children: <Widget>[
+              Container(
+                child: OnboardWidget(),
+              )
+            ],
+    );
   }
 }
