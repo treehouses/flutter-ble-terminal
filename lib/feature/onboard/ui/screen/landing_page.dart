c29724810369a87634f54be103473308bfea351d
John <computeronfire@protonmail.com>
2021-10-29 23:43:07 -0500

    cleanup of imports

--- lib/feature/onboard/ui/screen/landing_page.dart
+++ lib/feature/onboard/ui/screen/landing_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:treehousesble/common/bloc/bluetooth_cubit.dart';
 import 'package:treehousesble/common/bloc/bluetooth_state.dart';
-import 'package:treehousesble/feature/dashboard/screen/dashboard_page.dart';
 import 'package:treehousesble/feature/dashboard/screen/search_rpi_screen.dart';
 
 import 'onboard_page.dart';
@@ -29,7 +27,6 @@ class _LandingPageState extends State<LandingPage> {
       bloc: context.read<BluetoothCubit>(),
       builder: (context, state) {
         if (state is FirstTimeAppOpen) {
-
           return OnboardPage();
         } else {
           return SearchRpiScreen();
