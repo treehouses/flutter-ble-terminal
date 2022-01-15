3b15c13b41e1ecafe6c278405df178f35c6fae7e
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2022-01-15 11:38:15 +0800

    add scrollview

--- lib/feature/system/widget/system_home_widget.dart
+++ lib/feature/system/widget/system_home_widget.dart
@@ -3,86 +3,133 @@ import 'package:flutter/material.dart';
 class SystemHomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ExpansionTile(
-          title: Container(
-            child: Text("Shutdown & Reboot",),
-          ),
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
+    return
+      SingleChildScrollView(
+        child: Column(
+          children: [
+            ExpansionTile(
+              title: Container(
+                child: Text("Shutdown & Reboot",),
+              ),
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Image.asset(
-                      'assets/icons/shutdown.png',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/icons/shutdown.png',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          'Shutdown',
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Shutdown',
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/icons/restart.png',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          'Reboot',
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                Column(
-                  children: [
-                    Image.asset(
-                      'assets/icons/restart.png',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    Text(
-                      'Reboot',
-                    ),
-                  ],
-                ),
-              ],
+              ]
             ),
-          ]
-        ),
-        ExpansionTile(
-          title: Container(
-            child: Text("Open VNC"),
-          ),
-          children: <Widget>[
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                TextFormField(
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your Raspberry Pi\'s IP Adresss',
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0x00000000),
-                        width: 1,
-                      ),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(4.0),
-                        topRight: Radius.circular(4.0),
+            ExpansionTile(
+              title: Container(
+                child: Text("Open VNC"),
+              ),
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    TextFormField(
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your Raspberry Pi\'s IP Adresss',
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
                       ),
                     ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0x00000000),
-                        width: 1,
-                      ),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(4.0),
-                        topRight: Radius.circular(4.0),
-                      ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'VNC:',
+                        ),
+                        Switch(
+                            value: false,
+                            onChanged: null
+                        )
+                      ],
                     ),
-                  ),
-                ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      ),
+                      onPressed: () {
+                      },
+                      child: Text('START CONFIGURATION'),
+                    )
+                  ],
+                )
+              ]
+            ),
+            ExpansionTile(
+              title: Container(
+                child: Text("Toggle Camera"),
+              ),
+              children: <Widget>[
                 Row(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'VNC:',
+                    Row(
+                      children: [
+                        Text(
+                          'Camera:',
+                        ),
+                        Icon(
+                          Icons.video_call,
+                          color: Colors.black,
+                          size: 24,
+                        ),
+                      ],
                     ),
                     Switch(
                         value: false,
@@ -90,54 +137,10 @@ class SystemHomeWidget extends StatelessWidget {
                     )
                   ],
                 ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )
-                    ),
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                  ),
-                  onPressed: () {
-                  },
-                  child: Text('START CONFIGURATION'),
-                )
-              ],
-            )
-          ]
-        ),
-        ExpansionTile(
-          title: Container(
-            child: Text("Toggle Camera"),
-          ),
-          children: <Widget>[
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      'Camera:',
-                    ),
-                    Icon(
-                      Icons.video_call,
-                      color: Colors.black,
-                      size: 24,
-                    ),
-                  ],
-                ),
-                Switch(
-                    value: false,
-                    onChanged: null
-                )
-              ],
+              ]
             ),
-          ]
-        ),
-      ],
+          ],
+      )
     );
   }
 }
