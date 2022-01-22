cd7ca2e69cd53f6763085ec92e992f9bffe909b1
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2022-01-22 10:55:33 +0800

    add column and scrollview

--- lib/feature/system/widget/system_ssh_widget.dart
+++ lib/feature/system/widget/system_ssh_widget.dart
@@ -3,19 +3,21 @@ import 'package:flutter/material.dart';
 class SystemSSHWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ExpansionTile(
-            title: Container(
-                child: Text("Add SSH Key",)
-            )
-        ),
-        ExpansionTile(
-            title: Container(
-                child: Text("SSH 2 Factor Authentication")
-            )
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ExpansionTile(
+              title: Container(
+                  child: Text("Add SSH Key",)
+              )
+          ),
+          ExpansionTile(
+              title: Container(
+                  child: Text("SSH 2 Factor Authentication")
+              )
+          ),
+        ],
+      )
     );
   }
 }
