6e61c3ad09b8be074a907598a12116344caf2b33
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2022-01-30 23:42:14 +0800

    add rounded design and color

--- lib/feature/system/widget/system_ssh_widget.dart
+++ lib/feature/system/widget/system_ssh_widget.dart
@@ -8,179 +8,189 @@ class SystemSSHWidget extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ExpansionTile(
-                title: Container(
-                    child: Text("Add SSH Key",)
-                ),
-                children:<Widget>[
-                  Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Text(
-                            'SSH Key:',
-                          ),
-                          Expanded(
-                            child: TextFormField(
-                              obscureText: false,
-                              decoration: InputDecoration(
-                                hintText: 'My Key',
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0x00000000),
-                                    width: 1,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: ExpansionTile(
+                  backgroundColor: Colors.black12,
+                  collapsedBackgroundColor: Colors.black12,
+                  title: Container(
+                      child: Text("Add SSH Key",)
+                  ),
+                  children:<Widget>[
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text(
+                              'SSH Key:',
+                            ),
+                            Expanded(
+                              child: TextFormField(
+                                obscureText: false,
+                                decoration: InputDecoration(
+                                  hintText: 'My Key',
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color(0x00000000),
+                                      width: 1,
+                                    ),
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(4.0),
+                                      topRight: Radius.circular(4.0),
+                                    ),
                                   ),
-                                  borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(4.0),
-                                    topRight: Radius.circular(4.0),
-                                  ),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0x00000000),
-                                    width: 1,
-                                  ),
-                                  borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(4.0),
-                                    topRight: Radius.circular(4.0),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color(0x00000000),
+                                      width: 1,
+                                    ),
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(4.0),
+                                      topRight: Radius.circular(4.0),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              )
-                          ),
-                          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          ],
                         ),
-                        onPressed: () {
-                        },
-                        child: Text('SAVE KEY'),
-                      )
-                    ],
-                  ),
-                ]
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )
+                            ),
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          ),
+                          onPressed: () {
+                          },
+                          child: Text('SAVE KEY'),
+                        )
+                      ],
+                    ),
+                  ]
+              ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ExpansionTile(
-                title: Container(
-                    child: Text("SSH 2 Factor Authentication")
-                ),
-                children: <Widget>[
-                  Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Text(
-                            '2 Factor Authentication',
-                          ),
-                          Switch(
-                              value: false,
-                              onChanged: null
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Text(
-                            'Add or Remove User:',
-                          ),
-                          Expanded(
-                            child: TextFormField(
-                              obscureText: false,
-                              decoration: InputDecoration(
-                                hintText: 'pi',
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0x00000000),
-                                    width: 1,
-                                  ),
-                                  borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(4.0),
-                                    topRight: Radius.circular(4.0),
-                                  ),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Color(0x00000000),
-                                    width: 1,
-                                  ),
-                                  borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(4.0),
-                                    topRight: Radius.circular(4.0),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          ElevatedButton(
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  )
-                              ),
-                              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                            ),
-                            onPressed: () {
-                            },
-                            child: Text('ADD'),
-                          ),
-                          ElevatedButton(
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  )
-                              ),
-                              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                            ),
-                            onPressed: () {
-                            },
-                            child: Text('REMOVE'),
-                          ),
-                          ElevatedButton(
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  )
-                              ),
-                              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                            ),
-                            onPressed: () {
-                            },
-                            child: Text('SHOW'),
-                          )
-                        ],
-                      ),
-                    ],
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: ExpansionTile(
+                  backgroundColor: Colors.black12,
+                  collapsedBackgroundColor: Colors.black12,
+                  title: Container(
+                      child: Text("SSH 2 Factor Authentication")
                   ),
-                ]
+                  children: <Widget>[
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text(
+                              '2 Factor Authentication',
+                            ),
+                            Switch(
+                                value: false,
+                                onChanged: null
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text(
+                              'Add or Remove User:',
+                            ),
+                            Expanded(
+                              child: TextFormField(
+                                obscureText: false,
+                                decoration: InputDecoration(
+                                  hintText: 'pi',
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color(0x00000000),
+                                      width: 1,
+                                    ),
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(4.0),
+                                      topRight: Radius.circular(4.0),
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color(0x00000000),
+                                      width: 1,
+                                    ),
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(4.0),
+                                      topRight: Radius.circular(4.0),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    )
+                                ),
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                              ),
+                              onPressed: () {
+                              },
+                              child: Text('ADD'),
+                            ),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    )
+                                ),
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                              ),
+                              onPressed: () {
+                              },
+                              child: Text('REMOVE'),
+                            ),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    )
+                                ),
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                              ),
+                              onPressed: () {
+                              },
+                              child: Text('SHOW'),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ]
+              ),
             ),
           ),
         ],
