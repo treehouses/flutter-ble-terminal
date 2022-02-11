2cb3113ca637d84b46e62aff58d768fa55fcf099
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2022-02-12 07:19:44 +0800

    system: internet tab design (fixes #36) (#60)
    Co-authored-by: rrijal53 <rijalroshan94@gmail.com>

---
+++ lib/feature/system/widget/internet_blocking_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class InternetBlockingWidget extends StatefulWidget {
+  @override
+  State<InternetBlockingWidget> createState() => _InternetBlockingWidgetState();
+}
+
+class _InternetBlockingWidgetState extends State<InternetBlockingWidget> {
+  late double sliderValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ExpansionTile(
+          title: Container(
+            child: Text("Internet Blocking"),
+          ),
+          children: <Widget>[
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text(
+                      'Blocking Nothing',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Blocker Disabled',
+                    ),
+                    Slider(
+                      activeColor: Color(0x3474e0),
+                      inactiveColor: Color(0xFF9E9E9E),
+                      min: 0,
+                      max: 10,
+                      value: sliderValue,
+                      onChanged: (newValue) {
+                        setState(() {
+                          sliderValue = newValue;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Text(
+                  'Move slider to change blocking',
+                ),
+              ],
+            )
+          ]),
+    );
+  }
+}
