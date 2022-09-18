4c91ffb733ebcffad10580d79f64e7e7c1091f8f
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2022-09-18 13:04:01 +0800

    take out dead code

--- lib/feature/dashboard/screen/search_rpi_screen.dart
+++ lib/feature/dashboard/screen/search_rpi_screen.dart
@@ -93,7 +93,6 @@ class _SearchRpiScreenState extends State<SearchRpiScreen> {
                                           },
                                         );
                                       }
-                                      return Text(snapshot.data.toString());
                                     },
                                   ),
                                 ))
